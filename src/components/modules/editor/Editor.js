1bfb7d7332a3fdbf3e0a9b0c3b4e54e3e435744d
sinyk <hyeshin2530@gmail.com>
2020-10-16 11:43:53 +0900

    MOKI-41 image 핸들러 추가 완료

--- src/components/modules/editor/Editor.js
+++ src/components/modules/editor/Editor.js
@@ -1,25 +1,8 @@
-import React, { useState, useEffect, useMemo, useCallback } from 'react';
-import ReactQuill, { Quill } from 'react-quill';
+import React, { useState, useMemo, useCallback } from 'react';
+import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
 import api from '../../../api';
 import { API_IMAGE_UPLOAD } from '../../../constants/APIs';
-
-const BlockEmbed = Quill.import('blots/block/embed');
-class ImageBlot extends BlockEmbed {
-    static create(url) {
-        const node = super.create();
-        node.setAttribute('src', url);
-        node.setAttribute('controls', '');
-        return node;
-    }
-
-    static value(node) {
-        return node.getAttribute('src');
-    }
-}
-ImageBlot.blotName = 'image';
-ImageBlot.tagName = 'image';
-Quill.register(ImageBlot);
 
 function Editor() {
     const [state, setState] = useState({
@@ -28,37 +11,13 @@ function Editor() {
         reactQuillRef: null,
     });
 
-    const attachQuillRefs = function () {
-        console.log('attachQuillRefs');
-        // Ensure React-Quill reference is available:
-        // const { reactQuillRef } = state;
-        // if (!reactQuillRef) return;
-        // if (typeof reactQuillRef.getEditor !== 'function') return;
-        // const ref = reactQuillRef.getEditor();
-        // if (ref != null) {
-        //     setState({
-        //         ...state,
-        //         quillRef: ref,
-        //     });
-        // }
-    };
-
-    useEffect(() => {
-        attachQuillRefs();
-    }, []);
-
-    const imageHandler = () => {
-        const { reactQuillRef } = state;
-        // const quillRef = reactQuillRef.getEditor();
-        console.log(reactQuillRef);
-        // console.log(reactQuillRef.getEditor);
-        // console.log(quillRef);
+    const imageHandler = (el) => {
+        const quillInstance = el.quill;
         const input = document.createElement('input');
         input.setAttribute('type', 'file');
         input.click();
 
         input.onchange = (e) => {
-            console.log(e.target.files);
             const file = e.target.files[0];
 
             const formData = new FormData();
@@ -70,10 +29,17 @@ function Editor() {
                 data: formData,
             }).then((res) => {
                 if (res.data) {
-                    console.log('success');
-                    // quillRef.insertEmbed(0, 'image', res.data.url);
+                    console.log('image upload success');
+                    const range = quillInstance.getSelection();
+                    quillInstance.insertEmbed(
+                        range.index,
+                        'image',
+                        res.data.url
+                    );
+                    quillInstance.setSelection(range.index + 1);
+                    quillInstance.focus();
                 } else {
-                    console.log('success');
+                    console.log('image upload error');
                 }
             });
         };
@@ -98,7 +64,12 @@ function Editor() {
                     ['link', 'image', 'video'],
                     ['clean'],
                 ],
-                handlers: { image: imageHandler },
+                handlers: {
+                    image() {
+                        // this를 넘겨줘야 핸들러 onchange 함수에서 toolbar object에 접근할 수 있음
+                        imageHandler.apply(null, [this]);
+                    },
+                },
             },
             clipboard: { matchVisual: false },
         }),
@@ -125,10 +96,12 @@ function Editor() {
 
     const setRef = useCallback(
         (el) => {
-            if (el !== state.reactQuillRef) {
+            if (!el && el !== state.reactQuillRef) {
+                const ref = el.getEditor();
                 setState({
                     ...state,
                     reactQuillRef: el,
+                    quillRef: ref,
                 });
             }
         },
