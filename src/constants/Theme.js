3338b08f1f153e5204784cec48536f254446bd80
sinyk <hyeshin2530@gmail.com>
2020-11-10 22:23:14 +0900

    MOKI-49 #comment typo, font 테마 추가

--- src/constants/Theme.js
+++ src/constants/Theme.js
@@ -7,6 +7,22 @@
 import { createMuiTheme } from '@material-ui/core/styles';
 import { red } from '@material-ui/core/colors';
 
+import RalewayWoff2 from './fonts/Raleway-Regular.woff2';
+
+const raleway = {
+    fontFamily: 'Raleway',
+    fontStyle: 'normal',
+    fontDisplay: 'swap',
+    fontWeight: 400,
+    src: `
+    local('Raleway'),
+    local('Raleway-Regular'),
+    url(${RalewayWoff2}) format('woff2')
+  `,
+    unicodeRange:
+        'U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF',
+};
+
 const THEME = createMuiTheme({
     palette: {
         primary: {
@@ -21,6 +37,16 @@ const THEME = createMuiTheme({
         },
         pinned: {
             main: red[500],
+        },
+    },
+    typography: {
+        fontFamily: 'Raleway, Dotum, Apple SD Gothic Neo, sans-serif',
+    },
+    overrides: {
+        MuiCssBaseline: {
+            '@global': {
+                '@font-face': [raleway],
+            },
         },
     },
 });
