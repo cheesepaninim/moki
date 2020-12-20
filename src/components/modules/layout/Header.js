fea95d65adb4e838cf2e7f2772aee050d4831e09
cheesepaninim <hpw0310@gmail.com>
2020-12-21 02:15:52 +0900

    로그인, 로그아웃 alert 모듈 분리 작업중..

--- src/components/modules/layout/Header.js
+++ src/components/modules/layout/Header.js
@@ -3,8 +3,42 @@ import Typography from '@material-ui/core/Typography';
 import { NavLink } from 'react-router-dom';
 
 import Alert from '../modal/Alert';
+import {toggleAlert} from "../../../features/modal/alertSlice";
+import {Backdrop, Fade, Modal} from "@material-ui/core";
+import {useDispatch} from "react-redux";
 
 function Header() {
+    // TODO: alert props 이름들 수정
+    const btnTxt = "This is opener with btnTxt"
+    const dispatch = useDispatch();
+    const AlertBtn = (() => (
+        <button onClick={() => dispatch(toggleAlert(true))}>
+            This is opener with btnEle
+        </button>
+    ))
+    // const modalBody = (() => {
+    //     return (
+    //         <Modal
+    //             open={open}
+    //             BackdropComponent={Backdrop}
+    //         >
+    //             <Fade in={open}>
+    //                 <div>
+    //                     <h2 id="transition-modal-title">Transition modal</h2>
+    //                     <p id="transition-modal-description">react-transition-group animates me.</p>
+    //                     <button>TEST CLOSE</button>
+    //                 </div>
+    //             </Fade>
+    //         </Modal>
+    //     )
+    // })
+    const bodyHeader = "HEADER"
+    const bodyTxt = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi, consequuntur eaque enim fugiat illum iusto mollitia qui quidem repellat ullam! Doloremque eaque exercitationem fugit praesentium quia saepe ut, velit voluptatum."
+    const okTxt = "OK!"
+    const okBtn = (() => (
+        <button style={{'color':'red'}}>OK!</button>
+    ))
+
     return (
         <div>
             <Typography>Typography 테마 적용</Typography>
@@ -28,7 +62,18 @@ function Header() {
                         Etc
                     </NavLink>
                 </li>
-                <Alert/>
+                <li>
+                    <Alert
+                        btnTxt={btnTxt}
+                        {/* AlertBtn() 또는 <AlertBtn/> 으로 전달 가능 */}
+                        btn={AlertBtn()}
+                        /*body={modalBody}*/
+                        bodyHeader={bodyHeader}
+                        bodyTxt={bodyTxt}
+                        okText={okTxt}
+                        okBtn={okBtn}
+                    />
+                </li>
             </ul>
         </div>
     );
--- src/components/modules/modal/Alert.js
+++ src/components/modules/modal/Alert.js
@@ -1,17 +1,11 @@
 import React from 'react';
 import { useSelector, useDispatch } from "react-redux";
-import {
-    selectOpen,
-    toggleAlert
-} from "../../../features/modal/alertSlice";
-import {
-    Modal,
-    Fade,
-    Backdrop
-} from '@material-ui/core';
+import { selectOpen, toggleAlert } from "../../../features/modal/alertSlice";
+import { Modal, Fade, Backdrop } from '@material-ui/core';
 import { makeStyles } from "@material-ui/core/styles";
+import PropTypes from 'prop-types';
 
-function Alert() {
+function Alert(props) {
     const open = useSelector(selectOpen);
     const dispatch = useDispatch();
 
@@ -30,26 +24,42 @@ function Alert() {
     }));
     const classes = useStyles();
 
+    // TODO: husky 로 commit 시 prettier 적용
+
+    // TODO: 버튼 분기 처리
+    // Component 가 있는 경우 Component 로 & 없는 경우 btnTxt 로
+
     return (
         <>
             <button type="button" onClick={() => dispatch(toggleAlert(true))}>
-                Open Alert
+                {/*Open Alert*/}
+                {props.btnTxt}
             </button>
+            {props.btn}
             <Modal
                 open={open}
                 onClose={() => dispatch(toggleAlert(false))}
                 BackdropComponent={Backdrop}
+                className={classes.modal}
             >
                 <Fade in={open}>
                     <div className={classes.paper}>
                         <h2 id="transition-modal-title">Transition modal</h2>
                         <p id="transition-modal-description">react-transition-group animates me.</p>
-                        <button>TEST CLOSE</button>
+                        <button onClick={() => dispatch(toggleAlert(false))}>TEST CLOSE</button>
                     </div>
                 </Fade>
             </Modal>
         </>
     );
 }
+
+// TODO: alert props 타입 체크
+Alert.propTypes = {
+    // btnTxt: PropTypes.string.isRequired,
+    // color: PropTypes.string,
+    // title: PropTypes.string,
+    // addClassName: PropTypes.string,
+};
 
 export default Alert;
